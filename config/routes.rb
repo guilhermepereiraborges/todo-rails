36097631d423bd960f7ef9fa13a040f0d58a7697
Guilherme Borges <guilhermepereira@alu.ufc.br>
2026-01-14 00:38:14 -0300

    feat: define routes for TodoList and TodoItem with complete action

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,16 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  resources :todo_lists do
+    resources :todo_items do
+      member do
+        patch :complete
+      end
+    end
+  end
+
+  root "todo_lists#index"
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
