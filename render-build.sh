dceae2880a0a687049ddfeb6ec789b0841171f07
Guilherme Borges <guilhermepereira@alu.ufc.br>
2026-01-14 10:55:35 -0300

    feat: refactor database configuration and update build commands for production

--- render-build.sh
+++ render-build.sh
@@ -3,10 +3,9 @@
 set -o errexit
 
 bundle install
-bundle exec rails tailwindcss:build
 
-bundle exec rails assets:precompile
-
-bundle exec rails assets:clean
+SECRET_KEY_BASE_DUMMY=1 bundle exec rails tailwindcss:build
+SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:precompile
+SECRET_KEY_BASE_DUMMY=1 bundle exec rails assets:clean
 
 bundle exec rails db:migrate
